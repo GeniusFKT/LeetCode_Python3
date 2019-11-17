e7a67a23bd367a326fa9e270e7094f68e62f9698
Kenneth <yancheng1112@sjtu.edu.cn>
2019-11-17 17:01:32 +0800

    some answer

---
+++ 744_寻找比目标字母大的最小字母.py
@@ -0,0 +1,16 @@
+import bisect
+
+# linear searching
+# 140ms/76.69%
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for i in letters:
+            if i > target:
+                return i
+        return letters[0]
+
+# 220ms/13.78%
+class Solution1:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        index = bisect.bisect(letters, target)
+        return letters[index % len(letters)]
